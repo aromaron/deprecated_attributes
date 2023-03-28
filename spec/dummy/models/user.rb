8199dbf6c842ccba44af3f6c7fcfd6e2fe1d9dff
Nora Alvarado <nora.alvarado@hey.com>
2023-03-27 18:11:39 -0600

    fix: standard offense

--- spec/dummy/models/user.rb
+++ spec/dummy/models/user.rb
@@ -3,5 +3,5 @@
 class User < ActiveRecord::Base
   include DeprecatedAttributes
 
-  deprecated_attribute [:test_deprecated_attribute, :another_deprecated_attribute], message: 'test message'
+  deprecated_attribute [:test_deprecated_attribute, :another_deprecated_attribute], message: "test message"
 end
